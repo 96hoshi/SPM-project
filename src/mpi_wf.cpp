3566effa36907808bdd41ac6c33be269d329230e
96hoshi <m.locascio96@hotmail.com>
2024-07-20 18:55:27 +0200

    gatherv tests

--- src/mpi_wf.cpp
+++ src/mpi_wf.cpp
@@ -73,17 +73,19 @@ int main(int argc, char** argv) {
         int start_row = rank * rows_per_process + std::min(rank, remaining_rows);
         int end_row = start_row + rows_per_process + (rank < remaining_rows ? 1 : 0);
         printf("Rank %d: start_row = %d, end_row = %d\n", rank, start_row, end_row);
+        printf("Rank %d: rows_per_process = %d, remaining_rows = %d\n", rank, rows_per_process, remaining_rows);
 
-        std::vector<double> local_results(end_row - start_row, 0.0);
+        std::vector<double> local_results(end_row - start_row, 200.0 + rank);
 
-        // todo: mismatch in the indices i wanna cry
-        for (int i = start_row; i < end_row; ++i) {
-            if (i + k < N) {
-                double dot_product = compute_dot_product(&matrix[i * N], &matrix[(i + k) * N], k);
-                local_results[i - start_row] = compute_cubic_root(dot_product);
-                std::printf("Rank %d: M[%d][%d] = %f\n", rank, i, i + k, local_results[i - start_row]);
-            }
-        }
+        // TODO: mismatch in the indices i wanna cry dai cazzo
+        // for (int i = start_row; i < end_row; ++i) {
+        //     if (i + k < N) {
+        //         double dot_product = compute_dot_product(&matrix[i * N], &matrix[(i + k) * N], k);
+        //         local_results[i - start_row] = compute_cubic_root(dot_product);
+        //         std::printf("Rank %d: M[%d][%d] = %f\n", rank, i, i + k, local_results[i - start_row]);
+        //     }
+        // }
+
 
         // Gather results at the root process
         if (rank == 0) {
@@ -91,28 +93,38 @@ int main(int argc, char** argv) {
             std::vector<int> displs(size);
 
             for (int i = 0; i < size; ++i) {
+                // Compute the number of elements to receive from each process
                 recv_counts[i] = (N - k) / size + (i < remaining_rows ? 1 : 0);
+                // Compute the displacement for each process 
                 displs[i] = i * rows_per_process + std::min(i, remaining_rows);
             }
 
             std::vector<double> gathered_results(N - k, 0.0);
+            // Gather the results from all processes
             MPI_Gatherv(local_results.data(), local_results.size(), MPI_DOUBLE, gathered_results.data(), recv_counts.data(), displs.data(), MPI_DOUBLE, 0, MPI_COMM_WORLD);
+            printf("Rank %d: gathered_results.size() = %d\n", rank, gathered_results.size());
+            printf("Rank %d: gathered_results.data() = %p\n", rank, gathered_results.data());
 
             // Update the matrix with the gathered results
             for (int i = 0; i < N - k; ++i) {
                 matrix[i * N + (i + k)] = gathered_results[i];
             }
         } else {
+            // Send the results to the root process
             MPI_Gatherv(local_results.data(), local_results.size(), MPI_DOUBLE, nullptr, nullptr, nullptr, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
         }
         printf("Rank %d: Done\n", rank);
 
         // Broadcast updated matrix for next iteration
         MPI_Bcast(matrix, mat_size, MPI_DOUBLE, 0, MPI_COMM_WORLD);
     }
-
+    
     // Print the final matrix
     if (rank == 0) {
+        //MPI_Barrier(MPI_COMM_WORLD);
+
+        std::printf("Final matrix:\n");
         for (int i = 0; i < N; ++i) {
             for (int j = 0; j < N; ++j) {
                 std::printf("%f ", matrix[i * N + j]);
